59ec886757ca9cf7dceb3ea7b8ec0b37f1dbe7b2
TommasoVandermeer <tommasovdm@gmail.com>
2025-11-17 10:01:46 +0100

    add requirements.txt

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
+import os
+
+print("\n\nWARNING: This library for ORCA is still buggy and not very reliable. Use at your own risk!\n\n")
+
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+    jorca_requirements = f.read().splitlines()
 
 setup(
     name='jorca',
     version='0.0.1',
     packages= find_packages(),
-    install_requires = [
-        'jax',
-        'matplotlib',
-    ]
+    install_requires = jorca_requirements
 )
